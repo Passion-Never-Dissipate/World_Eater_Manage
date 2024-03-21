7b250c6dfb4b63f68facc459c38285724849a2dd
Bexerlmao <90677755+Bexerlmao@users.noreply.github.com>
2024-03-21 16:22:40 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,25 @@
 import json
 import os
 import codecs
+import time
 
 from mcdreforged.api.all import *
 
 bots_list = {}
+bots_restart_list = []
+pos3d = []
+dimension = 0
+time_out = 5
 
 Prefix = '!!we'
 help_msg = '''
 ------ {1} {2} ------
 一款专为技术生存服务器设计的世吞运维插件
-§3作者：Bexerlmao, FRUIT_CANDY
+§3作者：Bexerlmao, FRUIT_CANDY,SEEU_SAMA
 §d【格式说明】
 !!we 查看帮助信息
-!!we bot spawn <x1> <z1> <x2> <z2> <维度:0主世界,-1地狱,1末地> <服务器视距> <假人名> 坐标所围成的空置域生成一个世吞假人组假人
+!!we spawn <x1> <z1> <x2> <z2> <维度:0主世界,-1地狱,1末地> <服务器视距> <假人名> 坐标所围成的空置域生成假人
+!!we spawn <半径> <假人名> 以玩家为中心的半径内生成假人
 !!we kill <假人名> 删除该组假人并使其退出游戏
 !!we restart <假人名> 使该组假人在服务器重启后自动生成
 !!we list 查看正在运行的世吞假人组
@@ -24,47 +30,150 @@ def print_help_msg(source: CommandSource):
     source.reply(help_msg)
 
 
+def conversion_chunk(pos):
+    return pos//16
+
+def conversion_pos(chunk):
+    return chunk*16
+
 def spawn_bot(source: CommandSource, dic: dict):
+    if dic['dim'] not in [-1, 0, 1]:
+        source.reply(f"维度{dic['dim']}不存在")
+        return
+
+    if 'name' not in dic:
+        while True:
+            value = 1
+            try:
+                bots_list[f'w{value}']
+                value = value + 1
+            except:
+                dic['name'] = f'w{value}'
+                break
+
+    if dic['name'] in bots_list:
+        source.reply(f"{dic['name']}已存在")
+        return
+
     botid = 0
     bots_list[dic['name']] = {}
-    source.get_server().execute(f"/say {botid}")
+
+    if dic['dim'] == 0:
+        dim = 'overworld'
+    elif dic['dim'] == -1:
+        dim = 'nether'
+    elif dic['dim'] == 1:
+        dim = 'the_end'
+
     if dic['x2'] < dic['x1']:
         dic['x2'], dic['x1'] = dic['x1'], dic['x2']
     if dic['z2'] < dic['z1']:
         dic['z2'], dic['z1'] = dic['z1'], dic['z2']
-    for xpos in range(conversion_chunk(dic['x1']), conversion_chunk(dic['x2']), dic['view'] + 1):
-        for zpos in range(conversion_chunk(dic['z1']), conversion_chunk(dic['z2']), dic['view'] + 1):
-            source.get_server().execute(f"/player {dic['name']} spawn at {conversion_pos(xpos)} 100 {conversion_pos(zpos)} facing 0 0 in minecraft:overworld in spectator")
-            botid = botid + 1
-            bots_list[dic['name']][f"{dic['name']}{botid}"] = (xpos, zpos)
-    if botid == 0:
-        x = conversion_pos(dic['x1']) + abs(dic['x1'] - dic['x2']) / 2
-        z = conversion_pos(dic['z1']) + abs(dic['z1'] - dic['z2']) / 2
-        source.get_server().execute(f"/say {x} {z} {abs(dic['x1'] - dic['x2'])} ")
-        source.get_server().execute(f"/player {dic['name']} spawn at {x} 100 {z} facing 0 0 in minecraft:overworld in spectator")
-        botid = botid + 1
-        bots_list[dic['name']][f"{dic['name']}{botid}"] = (x, z)
 
-    source.get_server().execute(f"/say 成功生成{botid}个假人")
+    for xpos in range(conversion_chunk(dic['x1']) - dic['view'], conversion_chunk(dic['x2']), dic['view'] * 2 + 1):
+        for zpos in range(conversion_chunk(dic['z1'])- dic['view'], conversion_chunk(dic['z2']), dic['view'] * 2 + 1):
+            botid = botid + 1
+            source.get_server().execute(f"/player {dic['name']}_{botid} spawn at {conversion_pos(xpos)} 100 {conversion_pos(zpos)} facing 0 0 in minecraft:overworld in spectator")
+            bots_list[dic['name']][f"{dic['name']}_{botid}"] = (xpos, zpos)
+
+    if botid == 0:
+        x = conversion_pos(dic['x1']) + abs(dic['x1'] - dic['x2']) // 2
+        z = conversion_pos(dic['z1']) + abs(dic['z1'] - dic['z2']) // 2
+        botid = botid + 1
+        source.get_server().execute(f"/player {dic['name']}_{botid} spawn at {x} 100 {z} facing 0 0 in minecraft:{dim} in spectator")
+        bots_list[dic['name']][f"{dic['name']}_{botid}"] = (x, z)
+
+    source.reply(f"成功生成{botid}个假人")
+
+@new_thread("rspawn_bot")
+def rspawn_bot(source: CommandSource, dic: dict):
+    global pos3d, dimension
+
+    if dic['r'] < 0:
+        source.reply("半径应为大于等于0的整数!")
+        return
+
+    if source.get_info().player:
+        source.reply("正在获取玩家坐标")
+        source.get_server().execute(f"data get entity {source.get_info().player} Dimension")
+        source.get_server().execute(f"data get entity {source.get_info().player} Pos")
+
+    t1 = time.time()
+    while not pos3d:
+        time.sleep(0.1)
+        if time.time() - t1 > time_out:
+            source.reply("获取玩家坐标超时")
+            return
+
+
+        source.get_server().logger.info(pos3d)
+        source.get_server().logger.info(dimension)
+
+        user_xpos = conversion_chunk(int(float(pos3d[0])))
+        user_zpos = conversion_chunk(int(float(pos3d[2])))
+
+        dic['x1'] = user_xpos - dic['r']
+        dic['x2'] = user_xpos + dic['r']
+        dic['z1'] = user_zpos - dic['r']
+        dic['z2'] = user_zpos + dic['r']
+        dic['dim'] = dimension
+
+    spawn_bot(source,dic)
+    source.reply(f"只能由玩家进行该指令")
+
+@new_thread("get_info")
+def on_info(server: PluginServerInterface, info: Info):
+    global pos3d, dimension
+    if info.content.count("has the following entity data: ") and info.is_from_server:
+
+        if len(info.content.split(" ")) == 9 :
+            temp = [pos.rstrip('d').strip() for pos in info.content.split(":")[-1][2: -1].split(",")]
+            server.logger.info(temp)
+            pos3d.extend(temp)
+            return
+
+        if info.content.split(" ")[-1][0: -1] == "overworld":
+            dimension = 0
+        elif info.content.split(" ")[-1][0: -1] == "the_end":
+            dimension = 1
+        elif info.content.split(" ")[-1][0: -1] == "nether":
+            dimension = -1
+
 def kill_bot(source: CommandSource, dic: dict):
-    pass
+    for bot in bots_list[dic['name']]:
+        source.get_server().execute(f"/player {bot} kill")
+    del bots_list[dic['name']]
+    bots_restart_list.remove(dic['name'])
 
 def restart_bot(source: CommandSource, dic: dict):
-    pass
+    try:
+        bots_list[dic['name']]
+        bots_restart_list.append(dic['name'])
+        source.reply(f"添加{dic['name']}至重启列表")
+    except:
+        source.reply(f"{dic['name']}不存在")
 
 def list_bot(source: CommandSource, dic: dict):
-    pass
+    source.reply(f"正在运行的世吞假人组: {bots_list.keys()}")
 
 def reload_plugin(source: CommandSource):
     source.get_server().reload_plugin("world_eater_manage")
     source.reply("§a§l插件已重载")
 
 def on_load(server: PluginServerInterface, prev: PluginServerInterface):
+
+    for index in bots_restart_list:
+        for bot in bots_list[index]:
+            server.execute(f"/player {bot} spawn at {bots_list[index][bot][0]} 100 {bots_list[index][bot][1]} facing 0 0 in minecraft:overworld in spectator")
+
     builder = SimpleCommandBuilder()
 
     builder.command('!!we', print_help_msg)
     builder.command('!!we reload', reload_plugin)
     builder.command('!!we spawn <x1> <z1> <x2> <z2> <dim> <view> <name>', spawn_bot)
+    builder.command('!!we spawn <x1> <z1> <x2> <z2> <dim> <view>', spawn_bot)
+    builder.command('!!we rspawn <r> <view> <name>', rspawn_bot)
+    builder.command('!!we rspawn <r> <view>', rspawn_bot)
     builder.command('!!we kill <name>', kill_bot)
     builder.command('!!we restart <name>', restart_bot)
     builder.command('!!we list', list_bot)
@@ -76,12 +185,9 @@ def on_load(server: PluginServerInterface, prev: PluginServerInterface):
     builder.arg('dim', Integer)
     builder.arg('view', Integer)
     builder.arg('name', Text)
+    builder.arg('r', Integer)
 
     builder.register(server)
 
-def conversion_chunk(pos:Integer):
-    return pos//16
 
-def conversion_pos(chunk:Integer):
-    return chunk*16 + 8
 
