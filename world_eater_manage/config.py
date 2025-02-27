0be121c9a8b8c458e4e04230851a1f5181880339
FRUITS_CANDY <84139787+FRUITS-CANDY@users.noreply.github.com>
2025-02-27 16:56:04 +0800

    Update config.py

--- world_eater_manage/config.py
+++ world_eater_manage/config.py
@@ -9,6 +9,11 @@ class we_config(Serializable):
     gamemode: str = "spectator"
     group_name: str = "we"
     max_chunk_length: int = 51
+    dimension_list: Dict[str, str] = {
+        "0": "minecraft:overworld",
+        "1": "minecraft:the_nether",
+        "-1": "minecraft:the_end"
+    }
     minimum_permission_level: Dict[str, int] = {
         "spawn": 1,
         "rspawn": 1,
@@ -18,3 +23,4 @@ class we_config(Serializable):
         "clear": 1,
         "list": 0
     }
+    created_version: str = "1.4.0"
